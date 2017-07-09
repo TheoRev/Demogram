8d07c9c18775f0a976616918a81a53888dc20415
TheoRev <hrevillafernandez79@gmail.com>
2017-07-09 01:50:25 -0500

    Implementando CardView

--- app/src/main/java/com/theo/lheo2/demogram/view/ContainerActivity.java
+++ app/src/main/java/com/theo/lheo2/demogram/view/ContainerActivity.java
@@ -1,9 +1,16 @@
 package com.theo.lheo2.demogram.view;
 
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 import com.theo.lheo2.demogram.R;
+import com.theo.lheo2.demogram.view.fragment.HomeFragment;
+import com.theo.lheo2.demogram.view.fragment.ProfileFragment;
+import com.theo.lheo2.demogram.view.fragment.SearchFragment;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -11,5 +18,38 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottombar);
+        bottomBar.setDefaultTab(R.id.home);
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId) {
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    case R.id.profile:
+                        ProfileFragment profileFragment = new ProfileFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    case R.id.search:
+                        SearchFragment searchFragment = new SearchFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+
+                }
+            }
+        });
     }
 }
